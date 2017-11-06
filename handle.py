850c05c4c75f8274fbe3dae0b49b692c444ccc96
interface <627795061@qq.com>
2017-11-06 15:44:02 +0800

    reply image 2017年 11月 06日 星期一 15:44:02 CST

--- handle.py
+++ handle.py
@@ -14,9 +14,14 @@ class Handle(object):
             if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'text':
                 toUser = recMsg.FromUserName
                 fromUser = recMsg.ToUserName
-                content = "test"
+                content = "嗨，这么巧的!"
                 replyMsg = reply.TextMsg(toUser, fromUser, content)
                 return replyMsg.send()
+            else if isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'image':
+                toUser = recMsg.FromUserName
+                fromUser = recMsg.ToUserName
+                mediaId = recMsg.MediaId
+                replyMsg = reply.ImageMsg(toUser, fromUser, mediaId)
             else:
                 print "暂且不处理"
                 return "success"
