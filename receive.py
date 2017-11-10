fdb995f3c1a7d3ebde895b55c3fdfb7da272492c
interface <627795061@qq.com>
2017-11-10 17:01:57 +0800

    fix bug 2017年 11月 10日 星期五 17:01:57 CST

--- receive.py
+++ receive.py
@@ -12,7 +12,7 @@ def parse_xml(web_data):
     elif msg_type == 'image':
         return ImageMsg(xmlData)
 
-class Msg(object):
+class MsgText(object):
     def __init__(self, xmlData):
         self.ToUserName = xmlData.find('ToUserName').text
         self.FromUserName = xmlData.find('FromUserName').text
@@ -21,13 +21,21 @@ class Msg(object):
         self.MsgId = xmlData.find('MsgId').text
         self.Content = xmlData.find('Content').text
 
+class MsgImg(object):
+    def __init__(self, xmlData):
+        self.ToUserName = xmlData.find('ToUserName').text
+        self.FromUserName = xmlData.find('FromUserName').text
+        self.CreateTime = xmlData.find('CreateTime').text
+        self.MsgType = xmlData.find('MsgType').text
+        self.MsgId = xmlData.find('MsgId').text
+
 class TextMsg(Msg):
     def __init__(self, xmlData):
-        Msg.__init__(self, xmlData)
+        MsgText.__init__(self, xmlData)
         self.Content = xmlData.find('Content').text.encode("utf-8")
 
 class ImageMsg(Msg):
     def __init__(self, xmlData):
-        Msg.__init__(self, xmlData)
+        MsgImg.__init__(self, xmlData)
         self.PicUrl = xmlData.find('PicUrl').text
         self.MediaId = xmlData.find('MediaId').text
