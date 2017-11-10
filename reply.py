a66436f353d1c5724fd16a9e52633e813559ef4a
interface <627795061@qq.com>
2017-11-10 16:19:52 +0800

    test 2 2017年 11月 10日 星期五 16:19:51 CST

--- reply.py
+++ reply.py
@@ -29,14 +29,12 @@ class TextMsg(Msg):
         return XmlForm.format(**self.__dict)
     
 class ImageMsg(Msg):
-    def __init__(self, toUserName, fromUserName, mediaId, Content1, Content2):
+    def __init__(self, toUserName, fromUserName, mediaId):
         self.__dict = dict()
         self.__dict['ToUserName'] = toUserName
         self.__dict['FromUserName'] = fromUserName
         self.__dict['CreateTime'] = int(time.time())
         self.__dict['MediaId'] = mediaId
-        self.__dict['Content1'] = Content1
-        self.__dict['Content2'] = Content2
     def send(self):
         XmlForm = """
         <xml>
@@ -47,8 +45,6 @@ class ImageMsg(Msg):
         <Image>
         <MediaId><![CDATA[{MediaId}]]></MediaId>
         </Image>
-        <MsgType><![CDATA[text]]></MsgType>
-        <Content><![CDATA[{Content1}]]></Content>
         </xml>
         """
         return XmlForm.format(**self.__dict)
