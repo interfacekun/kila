b402644d1181a20978d48137866fa4dffd6675b8
interface <627795061@qq.com>
2017-11-10 17:15:18 +0800

    fix bug 2 2017年 11月 10日 星期五 17:15:18 CST

--- receive.py
+++ receive.py
@@ -12,16 +12,7 @@ def parse_xml(web_data):
     elif msg_type == 'image':
         return ImageMsg(xmlData)
 
-class MsgText(object):
-    def __init__(self, xmlData):
-        self.ToUserName = xmlData.find('ToUserName').text
-        self.FromUserName = xmlData.find('FromUserName').text
-        self.CreateTime = xmlData.find('CreateTime').text
-        self.MsgType = xmlData.find('MsgType').text
-        self.MsgId = xmlData.find('MsgId').text
-        self.Content = xmlData.find('Content').text
-
-class MsgImg(object):
+class Msg(object):
     def __init__(self, xmlData):
         self.ToUserName = xmlData.find('ToUserName').text
         self.FromUserName = xmlData.find('FromUserName').text
@@ -29,13 +20,16 @@ class MsgImg(object):
         self.MsgType = xmlData.find('MsgType').text
         self.MsgId = xmlData.find('MsgId').text
 
-class TextMsg(MsgText):
+        if self.MsgType == 'text':
+            self.Content = xmlData.find('Content').text
+
+class TextMsg(Msg):
     def __init__(self, xmlData):
-        MsgText.__init__(self, xmlData)
+        Msg.__init__(self, xmlData)
         self.Content = xmlData.find('Content').text.encode("utf-8")
 
-class ImageMsg(MsgImg):
+class ImageMsg(Msg):
     def __init__(self, xmlData):
-        MsgImg.__init__(self, xmlData)
+        Msg.__init__(self, xmlData)
         self.PicUrl = xmlData.find('PicUrl').text
         self.MediaId = xmlData.find('MediaId').text
